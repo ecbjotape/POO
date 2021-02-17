32b6bb077b050d5c600909475737934baf424a36
unknown <jpmoreiradejesus@hotmail.com>
2021-02-17 11:39:41 -0300

    Adicionando exercicio NotaAluno

---
+++ praticando_poo03/aplicacao03/Program03.java
@@ -0,0 +1,28 @@
+package aplicacao03;
+
+import java.util.Scanner;
+import entidades03.AlunoNota;
+
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		AlunoNota AlunoNota = new AlunoNota();
+		System.out.println("Digite o seu nome: ");
+		AlunoNota.nome = sc.nextLine();
+		System.out.println("Nota 1:");
+		AlunoNota.nota1 = sc.nextDouble();
+		System.out.println("Nota 2: ");
+		AlunoNota.nota2 = sc.nextDouble();
+		System.out.println("Nota 3: ");
+		AlunoNota.nota3 = sc.nextDouble();
+		
+		AlunoNota.calculoNota();
+		
+		
+		sc.close();
+	}
+
+}
---
+++ praticando_poo03/entidades03/AlunoNota.java
@@ -0,0 +1,34 @@
+package entidades03;
+
+public class AlunoNota {
+	
+	public double nota1;
+	public double nota2;
+	public double nota3;
+	public String nome;
+	
+	public double calculoNota(){
+		
+		double notaFinal = nota1 + nota2 + nota3 ;
+		if (notaFinal < 60) {
+			
+			System.out.println(nome);
+			System.out.println("NOTA FINAL: "+notaFinal);
+			System.out.println("REPROVADO");
+			notaFinal = 60 - notaFinal ;
+			System.out.println("Ponto(s) necessário(s) para aprovação: "+notaFinal);
+		}
+		else {
+			
+			System.out.println(nome);
+			System.out.println("NOTA FINAL: "+notaFinal);
+			System.out.println("APROVADO");	
+			
+		}
+		return 0;
+	}
+	
+	
+	
+	
+}
